4d4f5c5eb1aad1090f52d7d3b463a6f40d7a5e47
Joao-ava <ajoao781238@gmail.com>
2025-11-23 18:32:48 -0300

    inital commit

---
+++ config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :ride_fast, RideFast.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "ride_fast_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :ride_fast, RideFastWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "Ptp6tQMOYKhexB67aJGWlzWhr0A5csAXQId1qm3WKF/RVmhnBc7j/Lc5Ns34RAUy",
+  server: false
+
+# In test we don't send emails
+config :ride_fast, RideFast.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
